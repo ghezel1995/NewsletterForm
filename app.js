a7066e05f1c0aadacfbce0bed7f0422393353788
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-12-10 10:38:38 +0330

    updated the message section

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// Select the form with id='newsletterForm'
 document
   .getElementById('newsletterForm')
   .addEventListener('submit', function (event) {
     event.preventDefault();
-
+    // Get the email value entered by the user
     let email = document.getElementById('email').value;
     if (!email) {
       alert('Please enter your email');
@@ -10,6 +11,7 @@ document
     } else {
       console.log(email);
 
+      //Store the data in the localStorage
       let subscriber = { email: email };
       let subscribers = localStorage.getItem('subscribers');
       if (subscribers) {
@@ -20,7 +22,12 @@ document
       }
       localStorage.setItem('subscribers', JSON.stringify(subscribers));
 
+      // Display the success message for about 3 second
       document.getElementById('successMessage').style.display = 'block';
+
+      setTimeout(function () {
+        document.getElementById('successMessage').style.display = 'none';
+      }, 3000);
 
       document.getElementById('newsletterForm').reset();
     }
